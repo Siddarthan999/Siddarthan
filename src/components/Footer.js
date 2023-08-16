2b144c700993c718b36f6a7cac960e0263b38421
Siddarthan <91734840+Siddarthan999@users.noreply.github.com>
2023-08-16 21:35:00 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,9 +16,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/in/siddarthan-n-118635224/"><img src={navIcon1} alt="Icon" /></a>
-              <a href="https://www.facebook.com/siddarthan.natarajan/"><img src={navIcon2} alt="Icon" /></a>
-              <a href="https://www.instagram.com/_siddarthan_/"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/siddarthan-n-118635224/" target="_blank"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://www.facebook.com/siddarthan.natarajan/" target="_blank"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/_siddarthan_/" target="_blank"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright &copy; 2022. All Rights Reserved</p>
           </Col>
