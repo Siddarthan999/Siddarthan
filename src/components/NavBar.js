8c73072a927cc09088ff19ea71763cc958abcb83
Siddarthan <91734840+Siddarthan999@users.noreply.github.com>
2023-08-16 22:46:38 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../assets/img/logosid.png";
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon2 from '../assets/img/github-mark-white.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
@@ -51,7 +51,7 @@ export const NavBar = () => {
             <span className="navbar-text">
               <div className="social-icon">
                 <a href="https://www.linkedin.com/in/siddarthan-n-118635224/" target="_blank"><img src={navIcon1} alt="" /></a>
-                <a href="https://www.facebook.com/siddarthan.natarajan/" target="_blank"><img src={navIcon2} alt="" /></a>
+                <a href="https://github.com/Siddarthan999" target="_blank"><img src={navIcon2} alt="" /></a>
                 <a href="https://www.instagram.com/_siddarthan_/" target="_blank"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
