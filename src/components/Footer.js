183117094023b40b5d18868b80c0dc5998937bfa
Siddarthan <91734840+Siddarthan999@users.noreply.github.com>
2023-08-16 22:47:51 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logosid.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon2 from '../assets/img/github-mark-white.svg';
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
@@ -17,10 +17,10 @@ export const Footer = () => {
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/siddarthan-n-118635224/" target="_blank"><img src={navIcon1} alt="Icon" /></a>
-              <a href="https://www.facebook.com/siddarthan.natarajan/" target="_blank"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://github.com/Siddarthan999" target="_blank"><img src={navIcon2} alt="Icon" /></a>
               <a href="https://www.instagram.com/_siddarthan_/" target="_blank"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright &copy; 2023. All Rights Reserved</p>
+            <p>Copyright &copy; 2022. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
